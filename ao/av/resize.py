f5cbcdc6dc0b0924e0ff5c6ca205ceb220189ef7
Ari Makela <hauva@iki.fi>
2012-08-18 20:15:58 +0300

    user data corrected

---
+++ ao/av/resize.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+from __future__ import division
+
+import Image
+import pyexiv2
+
+import errno
+import getopt
+import os
+import os.path
+import sys
+
+def scale(image, size):
+    '''Returns a scaled image where the length of the longest edge is size.'''
+    w, h = image.size
+    if w == h:
+        w = size
+        h = size
+    elif w > h:
+        h = int(size * h / w)
+        w = size
+    else:
+        w = int(size * w/h)
+        h = size
+    return image.resize((w, h), Image.ANTIALIAS)
+
+if __name__ == '__main__':
+    frmd = sys.argv[1]
+    tod = sys.argv[2]
+    files = os.listdir(frmd)
+    for i in files:
+        fn = '%s/%s' % (frmd, i)
+        try:
+            im = Image.open(fn)
+        except IOError, ex:
+            # FIXME
+	    continue
+        out = scale(im, 800)
+        outfn = '%s/%s' % (tod, i)
+        out.save(outfn)
+
