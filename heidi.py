572a81c11c86e645eb842818b37c08f03ee8c5b1
Ari Makela <hauva@iki.fi>
2012-08-26 08:11:54 +0300

    os.path.walk, os.mkdir

--- heidi.py
+++ heidi.py
@@ -1,1 +1,29 @@
+#!/usr/bin/python
+
+# Heidin pyytama koko:
 # 1778x1181
+
+import errno
+import os
+import os.path
+
+DN = '/home/hauva/Pictures/2012/New_York/08'
+HDN = '/home/hauva/Pictures/heidille'
+
+def visit(arg, dname, fnames):
+    if dname.endswith('/cand'):
+        print dname
+
+def main():
+    try:
+        os.mkdir(HDN)
+    except OSError, ex:
+        if ex.errno == errno.EEXIST:
+            pass
+        else:
+            raise ex
+    os.path.walk(DN, visit, None)
+
+if __name__ == '__main__':
+    main()
+
