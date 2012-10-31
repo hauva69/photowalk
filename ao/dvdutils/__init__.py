04d42226ae3277a44c1fad838cd910644f306261
Ari Makela <hauva@iki.fi>
2012-10-31 16:38:20 +0200

    Transformed into a python script.

--- ao/dvdutils/__init__.py
+++ ao/dvdutils/__init__.py
@@ -1,2 +1,16 @@
+#!/usr/bin/python
+
 #http://ubuntuforums.org/showthread.php?t=363666
 # growisofs -dvd-compat -Z /dev/sr0=/path/to/image.iso
+
+import ConfigParser
+import os
+
+CONF = '%s/.aburnrc' % (os.environ['HOME'])
+
+def main():
+    print CONF
+
+if __name__ == '__main__':
+    main()
+
