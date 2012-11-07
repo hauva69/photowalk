f2032fde93a8b4e31796c0b59deaf811fc89fa32
Ari Makela <hauva@iki.fi>
2012-11-07 23:56:36 +0200

    Works as specified.

--- ao/av/rotate.py
+++ ao/av/rotate.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python
 
+import Image
 import pyexiv2
 
 import sys
 
 ORIENTATION_KEY = 'Exif.Image.Orientation'
 
-def rotate(filename):
+def rotate(image, filename):
+    '''Image, filename -> image rotated according to ORIENTATION_KEY.'''
     md = pyexiv2.ImageMetadata(filename)
     md.read()
-    print md[ORIENTATION_KEY].value
-    
+    print filename, md[ORIENTATION_KEY].value
+    o = md[ORIENTATION_KEY].value
+    rotation = 0
+    if o == 8: # left-bottom according to exif command.
+        rotation = 90
+    elif o == 6: # right-top accordin to exif command.
+        rotation = 270
+    elif o == 1:
+        rotation = 180
+    return image.rotate(rotation)
 
 if __name__ == '__main__':
-    rotate(sys.argv[1])
+    fn = sys.argv[1]
+    im = Image.open(fn)
+    im = rotate(im, fn)
+    im.show()
