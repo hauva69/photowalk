dd8e16e0f72f5609db66857df1291c80f544efb9
Ari Makela <hauva@iki.fi>
2012-11-14 01:07:01 +0200

    Added the interpreter and main().

--- ao/av/__init__.py
+++ ao/av/__init__.py
@@ -1,6 +1,22 @@
+#!/usr/bin/python
+
 PICASA_MAX_SIZE = 2048
 
 import Image
+import sys
 
 class PhotowalkImage(object):
-    
+    image = None
+    filename = None
+
+    def __init__(self, filename):
+        image = Image.open(filename)
+        self.filename = filename
+
+def main():
+    filenames = sys.argv[1:]
+    for i in filenames:
+        print i
+
+if __name__ == '__main__':
+    main()
