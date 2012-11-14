d8ddf48c84a1ba5ba2f835dc29b2558645a96f75
Ari Makela <hauva@iki.fi>
2012-11-14 02:02:20 +0200

    Print all metadata.

--- ao/av/__init__.py
+++ ao/av/__init__.py
@@ -14,21 +14,36 @@ class PhotowalkImage(object):
     metadata = None
 
     def __init__(self, filename):
-        '''filename -> PhotowalkImage'''
+        '''filename -> PhotowalkImage
+
+        Throws IOError if file type is not identified.'''
         self.image = Image.open(filename)
         self.filename = filename
         self.metadata = pyexiv2.ImageMetadata(filename)
+        self.metadata.read()
 
     def __str__(self):
         s = '''Filename: %s
 ''' % (self.filename)
+
+        for k, v in self.metadata.iteritems():
+            s += '%s: %s\n' % (k, str(v.value))
         return s
 
 def main():
+    rc = 0
+    prog = sys.argv[0]
     filenames = sys.argv[1:]
     for i in filenames:
-        pwim = PhotowalkImage(i)
-        print(pwim)
+        try:
+            pwim = PhotowalkImage(i)
+            print(pwim)
+        except IOError, ex:
+            msg = '%s: %s: %s\n' % (prog, i, str(ex))
+            sys.stderr.write(msg)
+            rc += 1
+
+    sys.exit(rc)
 
 if __name__ == '__main__':
     main()
