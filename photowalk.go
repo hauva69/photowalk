5915c4b6ba2e563c2134f03a8c71948c769a7ec4
Ari Makela <hauva@iki.fi>
2014-07-18 00:15:21 +0300

    handleFile(string, FileInfo) added

--- photowalk.go
+++ photowalk.go
@@ -3,10 +3,31 @@ package main
 import (
 	"fmt"
 	"github.com/hauva69/photowalk/logging"
-//	"io/ioutil"
+	"io/ioutil"
 	"os"
 )
 
+func handleFile(sourceDirectory string, file os.FileInfo) {
+	fn := fmt.Sprintf("%s/%s", sourceDirectory, file.Name())
+	logging.Log.Debug("filename=%s", fn)
+}
+
 func main() {
-	logging.Log.Info(os.Args)
+	sourceDirectory := os.Args[1]
+	logging.Log.Debug("src=%v", sourceDirectory)
+	files, err := ioutil.ReadDir(sourceDirectory)
+	if err != nil {
+		logging.Log.Fatal("%v", err)
+		os.Exit(1)
+	}
+
+	for i := range files {
+		f := files[i]
+		logging.Log.Debug("file=%s", f.Name())
+		if f.IsDir() {
+			logging.Log.Warning("%s is a directory", f)
+		} else {
+			handleFile(sourceDirectory, f)
+		}
+	}
 }
