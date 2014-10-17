2757300caf04f62baf2c07592a54fad4e0fe3d4a
Ari Makela <ari.makela@kauppalehti.fi>
2014-10-17 13:29:03 +0300

    Started fixing tests.

--- photograph/photograph_test.go
+++ photograph/photograph_test.go
@@ -2,7 +2,6 @@ package photograph
 
 import (
 	"fmt"
-	"sort"
 	"testing"
 )
 
@@ -10,20 +9,24 @@ func TestNew(t *testing.T) {
 	const image = "/path/to/image/photowalk.png"
 	const width, height = 13, 42
 	im := New()
+	im.OriginalFileName = image
+	im.Width = width
+	im.Height = height
 
-	if im.OriginalFileName != "" {
-		t.Errorf("OriginalFileName = %v, want an empty string", im.OriginalFileName)
+	if im.OriginalFileName != image {
+		t.Errorf("OriginalFileName = %s, want %s",
+			im.OriginalFileName, image)
 	}
 
-	if im.Width != 0 {
-		t.Errorf("Width = %v, want 0", im.Width)
+	if im.Width != width {
+		t.Errorf("Width = %v, want %d", im.Width, width)
 	}
 
-	if im.Height != 0 {
-		t.Errorf("Height = %v, want 0", im.Height)
+	if im.Height != height {
+		t.Errorf("Height = %v, want 0", im.Height, height)
 	}
 
-	im = &Photograph{image, width, height}
+	im = New()
 
 	if im.OriginalFileName != image {
 		t.Errorf("OriginalFileName = %v, want %v", im.OriginalFileName, image)
@@ -38,30 +41,8 @@ func TestNew(t *testing.T) {
 	}
 }
 
-func TestSortByMaximumDimension(t *testing.T) {
-	images := []Photograph{
-		{"/images/3.nef", 3000, 356},
-		{"/images/1.jpg", 123, 1000},
-		{"/images/2.png", 223, 2000},
-	}
-
-	sortedImages := []Photograph{
-		{"/images/1.jpg", 123, 1000},
-		{"/images/2.png", 223, 2000},
-		{"/images/3.nef", 3000, 356},
-	}
-
-	sort.Sort(ByMaximumDimension(images))
-
-	for i := 0; i < len(images); i++ {
-		if images[i] != sortedImages[i] {
-			t.Errorf("images[%d] = %v, want %v", i, images[i], sortedImages[i])
-		}
-	}
-}
-
 func TestString(t *testing.T) {
-	photo := Photograph{"/images/1.jpg", 1000, 2000}
+	photo := New()
 	expected := "/images/1.jpg\t1000\t2000"
 	got := fmt.Sprintf("%v", photo)
 	if got != expected {
