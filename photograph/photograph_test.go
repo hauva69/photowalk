8ba4f3a97b4c7b54495b9edd2fb97fcd5196c6ef
Ari Makela <ari.makela@kauppalehti.fi>
2014-10-19 04:48:12 +0300

    TestString now passes

--- photograph/photograph_test.go
+++ photograph/photograph_test.go
@@ -2,7 +2,9 @@ package photograph
 
 import (
 	"fmt"
+	"log"
 	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
@@ -29,8 +31,20 @@ func TestNew(t *testing.T) {
 }
 
 func TestString(t *testing.T) {
+	const timeFormat = "2006-01-02 15:04:05"
+	const timeString = "2006-01-02 15:04:05"
 	photo := New()
-	expected := "/images/1.jpg\t1000\t2000"
+	photo.OriginalFileName = "/images/1.jpg"
+	photo.Width = 1000
+	photo.Height = 2000
+	tmp, err := time.Parse(timeFormat, timeString)
+	if err != nil {
+		log.Fatal("Unable to parse date.")
+	} else {
+		photo.Time = tmp
+	}
+	expected := "/images/1.jpg\t1000\t2000\t" + timeString +
+		" +0000 UTC"
 	got := fmt.Sprintf("%v", photo)
 	if got != expected {
 		t.Errorf("expected = %v, want %v", expected, got)
