e78e9dcd803f64440e8944ffff40a6642f750178
Ari Makela <ari.makela@iki.fi>
2016-10-27 16:51:56 +0300

    Forgotten commit

--- gdataclient.py
+++ gdataclient.py
@@ -8,10 +8,15 @@ https://developers.google.com/picasa-web/docs/1.0/developers_guide_python
 
 import gdata.photos.service
 
+import sys
 
 def main():
     '''The main function.'''
     username = 'hauva69@gmail.com'
+    try:
+        username = sys.argv[1]
+    except:
+        pass
     gd_client = gdata.photos.service.PhotosService()
     albums = gd_client.GetUserFeed(user=username)
     for album in albums.entry:
